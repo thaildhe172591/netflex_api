5fdc9658a9a022d92e12e71d00050ab939eec8ca
thaildhe172591 <thaildhe172591@fpt.edu.vn>
2025-06-12 02:06:32 +0700

    update

--- src/Presentation/Netflex.WebAPI/Endpoints/V1/Keywords/GetKeywordsEndpoint.cs
+++ src/Presentation/Netflex.WebAPI/Endpoints/V1/Keywords/GetKeywordsEndpoint.cs
@@ -2,7 +2,7 @@ using Netflex.Shared.Pagination;
 
 namespace Netflex.WebAPI.Endpoints.V1.Keywords;
 
-public record GetKeywordsRequest(string? Query, PaginationRequest? Pagination);
+public record GetKeywordsRequest(string? Query) : PaginationRequest;
 public record GetKeywordsResponse(PaginatedResult<string> Keywords);
 
 public class GetKeywordsEndpoint : ICarterModule
