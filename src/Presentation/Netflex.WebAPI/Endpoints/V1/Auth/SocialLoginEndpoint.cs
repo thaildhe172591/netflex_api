2501095549bf59d97da3781a9b112fb6ebfc0a52
thaildhe172591 <thaildhe172591@fpt.edu.vn>
2025-06-19 00:45:01 +0700

    update

--- src/Presentation/Netflex.WebAPI/Endpoints/V1/Auth/SocialLoginEndpoint.cs
+++ src/Presentation/Netflex.WebAPI/Endpoints/V1/Auth/SocialLoginEndpoint.cs
@@ -3,15 +3,16 @@ using Netflex.Application.UseCases.V1.Auth.Queries;
 namespace Netflex.WebAPI.Endpoints.V1.Auth;
 
 public record SocialLoginRequest(string RedirectUrl);
+public record SocialLoginResponse(string LoginUrl);
 public class SocialLoginEndpoints : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapGet("/auth/{loginProvider}", async (string loginProvider,
+        app.MapPost("/auth/{loginProvider}", async (string loginProvider,
             [AsParameters] SocialLoginRequest request, ISender sender) =>
         {
             var result = await sender.Send(new GetSocialLoginQuery(loginProvider, request.RedirectUrl));
-            return Results.Redirect(result.LoginUrl);
+            return Results.Ok(new SocialLoginResponse(result.LoginUrl));
         })
         .MapToApiVersion(1)
         .WithName(nameof(SocialLoginEndpoints));
