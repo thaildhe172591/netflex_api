af6d5fa83f9b75163c8caabcec4de934380692d6
thaildhe172591 <thaildhe172591@fpt.edu.vn>
2025-07-26 10:19:51 +0700

    update

--- src/Core/Netflex.Application/UseCases/V1/Users/Commands/AssignRoleHandler.cs
+++ src/Core/Netflex.Application/UseCases/V1/Users/Commands/AssignRoleHandler.cs
@@ -22,14 +22,16 @@ public class AssignRoleHandler(
 
     public async Task<Unit> Handle(AssignRoleCommand request, CancellationToken cancellationToken)
     {
-        var user = _unitOfWork.Repository<Domain.Entities.User>().Get(request.UserId)
+        var user = await _unitOfWork.Repository<Domain.Entities.User>()
+            .GetAsync(u => u.Id == request.UserId, includeProperties: ["Roles"], cancellationToken: cancellationToken)
             ?? throw new UserNotFoundException();
 
         var role = await _unitOfWork.Repository<Domain.Entities.Role>()
             .GetAsync(r => r.Name == request.RoleName, cancellationToken: cancellationToken)
             ?? throw new NotFoundException(nameof(Domain.Entities.Role), request.RoleName);
 
-        user.AssignRole([role]);
+        if (user.Roles.Any(r => r.Name == role.Name))
+            user.AssignRole([role]);
         await _unitOfWork.CommitAsync();
         return Unit.Value;
     }
